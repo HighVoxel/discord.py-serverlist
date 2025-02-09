c897b3bd626f464c35e7052172f5c8079b1cf3de
HighVoxel <resentslbm@gmail.com>
2025-02-09 14:43:31 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord 
+from discord.ext import commands
+import discord
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.command()
+async def serverlist(ctx):
+    for guild in bot.guilds:
+        await ctx.send(await guild.text_channels[0].create_invite(max_age=0, max_uses=0, unique=True))
+    await ctx.send("全サーバーの招待リンクを取得しました")
+
+bot.run("Token")
